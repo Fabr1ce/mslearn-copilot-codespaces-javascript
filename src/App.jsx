cd5da103a7dbfa15f57c662fccaab1cd0e7621b5
Fabrice <neo.oseis@gmail.com>
2024-10-17 03:09:13 +0000

    initial copilot commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fabrice Eureka",
+  title: "DevOps Engineer",
+  email: "fabrice@example.com",
+  gitHub: "Fabr1ce",
+  linkedIn: "fabricee",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
